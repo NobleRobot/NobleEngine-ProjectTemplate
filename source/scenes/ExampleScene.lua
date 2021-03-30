eb7181d02ad3f54ee7246763cbd27ef9f6e13363
Mark LaCroix <mark@noblerobot.com>
2021-03-30 05:57:59 -0500

    Updated example scenes to use Noble.Menu objects.

--- source/scenes/ExampleScene.lua
+++ source/scenes/ExampleScene.lua
@@ -16,78 +16,47 @@ function ExampleScene:init()
 	background = Graphics.image.new("assets/images/background1")
 	logo = Graphics.image.new("libraries/noble/assets/images/NobleRobotLogo")
 
-	local menuItemKeys = {
-		Noble.TransitionType.DIP_TO_BLACK,
-		Noble.TransitionType.DIP_TO_WHITE,
-		Noble.TransitionType.DIP_METRO_NEXUS,
-		Noble.TransitionType.DIP_WIDGET_SATCHEL,
-		Noble.TransitionType.CROSS_DISSOLVE,
-		Noble.TransitionType.SLIDE_OFF_UP,
-		Noble.TransitionType.SLIDE_OFF_DOWN,
-		Noble.TransitionType.SLIDE_OFF_LEFT,
-		Noble.TransitionType.SLIDE_OFF_RIGHT,
+	menu = Noble.Menu.new(true, Noble.Text.ALIGN_LEFT, false, Graphics.kColorWhite, 4,6,0, Noble.Text.FONT_SMALL)
+
+	menu:addItem(Noble.TransitionType.DIP_TO_BLACK, function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.DIP_TO_BLACK) end)
+	menu:addItem(Noble.TransitionType.DIP_TO_WHITE, function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.DIP_TO_WHITE) end)
+	menu:addItem(Noble.TransitionType.DIP_METRO_NEXUS, function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.DIP_METRO_NEXUS) end)
+	menu:addItem(Noble.TransitionType.DIP_WIDGET_SATCHEL, function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.DIP_WIDGET_SATCHEL) end)
+	menu:addItem(Noble.TransitionType.CROSS_DISSOLVE, function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.CROSS_DISSOLVE) end)
+	menu:addItem(Noble.TransitionType.SLIDE_OFF_UP, function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.SLIDE_OFF_UP) end)
+	menu:addItem(Noble.TransitionType.SLIDE_OFF_DOWN, function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.SLIDE_OFF_DOWN) end)
+	menu:addItem(Noble.TransitionType.SLIDE_OFF_LEFT, function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.SLIDE_OFF_LEFT) end)
+	menu:addItem(Noble.TransitionType.SLIDE_OFF_RIGHT, function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.SLIDE_OFF_RIGHT) end)
+	menu:addItem(
 		"Difficulty: " .. Noble.Settings.get("Difficulty"),
-	}
-	local difficultyValues = { "Rare", "Medium", "Well Done" }
-	local menuHandlers = {
-		function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.DIP_TO_BLACK) end,
-		function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.DIP_TO_WHITE) end,
-		function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.DIP_METRO_NEXUS) end,
-		function() Noble.transition(ExampleScene2, 1.5, Noble.TransitionType.DIP_WIDGET_SATCHEL) end,
-		function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.CROSS_DISSOLVE) end,
-		function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.SLIDE_OFF_UP) end,
-		function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.SLIDE_OFF_DOWN) end,
-		function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.SLIDE_OFF_LEFT) end,
-		function() Noble.transition(ExampleScene2, 1, Noble.TransitionType.SLIDE_OFF_RIGHT) end,
 		function()
 			local newValue = math.ringInt(table.indexOfElement(difficultyValues, Noble.Settings.get("Difficulty"))+1, 1, 3)
 			Noble.Settings.set("Difficulty", difficultyValues[newValue])
 			menuItemKeys[10] = "Difficulty: " .. difficultyValues[newValue]
 		end
-	}
-
-	menu = UI.gridview.new(190, 10)
-	menu:setNumberOfColumns(1)
-	menu:setNumberOfRows(10)
-	menu:setCellPadding(0, 0, 0, 0)
-	menu.changeRowOnColumnWrap = false
-	menu:setSelectedRow(0)
-
-	function menu:drawCell(section, row, column, selected, x, y, width, height)
-		Noble.Text.setFont(Noble.Text.small)
-		if selected then
-			Graphics.setPattern({0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55})
-			Graphics.setLineWidth(5)
-			Graphics.drawLine(x, y+6, x+45, y+6)
-			Graphics.setLineWidth(1)
-			Graphics.setColor(Graphics.kColorWhite)
-			Graphics.drawLine(x, y+6, x+45, y+6)
-		end
-		Graphics.setImageDrawMode(Graphics.kDrawModeFillWhite)
-		Noble.Text.draw(menuItemKeys[row], x+50, y+2)
-	end
+	)
 
 	local crankTick = 0
 
 	ExampleScene.inputHandler = {
 		upButtonDown = function()
-			menu:selectPreviousRow(true, false, false)
+			menu:selectPrevious()
 		end,
 		downButtonDown = function()
-			menu:selectNextRow(true, false, false)
+			menu:selectNext()
 		end,
 		cranked = function(change, acceleratedChange)
 			crankTick = crankTick + change
 			if (crankTick > 30) then
 				crankTick = 0
-				menu:selectNextRow(true, false, false)
+				menu:selectNext()
 			elseif (crankTick < -30) then
 				crankTick = 0
-				menu:selectPreviousRow(true, false, false)
+				menu:selectPrevious()
 			end
 		end,
 		AButtonDown = function()
-			menuHandlers[menu.selectedRow]()
+			menu:click()
 		end
 	}
 
@@ -113,20 +82,20 @@ function ExampleScene:drawBackground()
 	ExampleScene.super.drawBackground(self)
 
 	background:draw(0, 0)
-	Graphics.setColor(Graphics.kColorBlack)
-	Graphics.fillRoundRect(15, (sequence:get()*0.75)+15, 185, 125, 15)
-
-	Graphics.setColor(Graphics.kColorWhite)
-	Graphics.fillRoundRect(260, -20, 130, 65, 15)
-	logo:setInverted(true)
-	logo:draw(275, 8)
-
 end
 
 function ExampleScene:update()
 	ExampleScene.super.update(self)
 
-	menu:drawInRect(15, sequence:get() or 100, 200, 110)
+	Graphics.setColor(Graphics.kColorBlack)
+	Graphics.setDitherPattern(0.2, Graphics.image.kDitherTypeScreen)
+	Graphics.fillRoundRect(15, (sequence:get()*0.75)+3, 185, 145, 15)
+	menu:draw(30, sequence:get()-15 or 100-15)
+
+	Graphics.setColor(Graphics.kColorWhite)
+	Graphics.fillRoundRect(260, -20, 130, 65, 15)
+	logo:setInverted(true)
+	logo:draw(275, 8)
 
 end
 
--- source/scenes/ExampleScene2.lua
+++ source/scenes/ExampleScene2.lua
@@ -15,77 +15,47 @@ function ExampleScene2:init()
 	background = Graphics.image.new("assets/images/background2")
 	logo = Graphics.image.new("libraries/noble/assets/images/NobleRobotLogo")
 
-	local menuItemKeys = {
-		Noble.TransitionType.DIP_TO_BLACK,
-		Noble.TransitionType.DIP_TO_WHITE,
-		Noble.TransitionType.DIP_METRO_NEXUS,
-		Noble.TransitionType.DIP_WIDGET_SATCHEL,
-		Noble.TransitionType.CROSS_DISSOLVE,
-		Noble.TransitionType.SLIDE_OFF_UP,
-		Noble.TransitionType.SLIDE_OFF_DOWN,
-		Noble.TransitionType.SLIDE_OFF_LEFT,
-		Noble.TransitionType.SLIDE_OFF_RIGHT,
+	menu = Noble.Menu.new(true, Noble.Text.ALIGN_LEFT, false, Graphics.kColorBlack, 4,6,0, Noble.Text.FONT_SMALL)
+
+	menu:addItem(Noble.TransitionType.DIP_TO_BLACK, function() Noble.transition(ExampleScene, 1, Noble.TransitionType.DIP_TO_BLACK) end)
+	menu:addItem(Noble.TransitionType.DIP_TO_WHITE, function() Noble.transition(ExampleScene, 1, Noble.TransitionType.DIP_TO_WHITE) end)
+	menu:addItem(Noble.TransitionType.DIP_METRO_NEXUS, function() Noble.transition(ExampleScene, 1, Noble.TransitionType.DIP_METRO_NEXUS) end)
+	menu:addItem(Noble.TransitionType.DIP_WIDGET_SATCHEL, function() Noble.transition(ExampleScene, 1, Noble.TransitionType.DIP_WIDGET_SATCHEL) end)
+	menu:addItem(Noble.TransitionType.CROSS_DISSOLVE, function() Noble.transition(ExampleScene, 1, Noble.TransitionType.CROSS_DISSOLVE) end)
+	menu:addItem(Noble.TransitionType.SLIDE_OFF_UP, function() Noble.transition(ExampleScene, 1, Noble.TransitionType.SLIDE_OFF_UP) end)
+	menu:addItem(Noble.TransitionType.SLIDE_OFF_DOWN, function() Noble.transition(ExampleScene, 1, Noble.TransitionType.SLIDE_OFF_DOWN) end)
+	menu:addItem(Noble.TransitionType.SLIDE_OFF_LEFT, function() Noble.transition(ExampleScene, 1, Noble.TransitionType.SLIDE_OFF_LEFT) end)
+	menu:addItem(Noble.TransitionType.SLIDE_OFF_RIGHT, function() Noble.transition(ExampleScene, 1, Noble.TransitionType.SLIDE_OFF_RIGHT) end)
+	menu:addItem(
 		"Change Score: " .. Noble.GameData.get("Score"),
-	}
-	local menuHandlers = {
-		function() Noble.transition(ExampleScene, 1, Noble.TransitionType.DIP_TO_BLACK) end,
-		function() Noble.transition(ExampleScene, 1, Noble.TransitionType.DIP_TO_WHITE) end,
-		function() Noble.transition(ExampleScene, 1, Noble.TransitionType.DIP_METRO_NEXUS) end,
-		function() Noble.transition(ExampleScene, 1.5, Noble.TransitionType.DIP_WIDGET_SATCHEL) end,
-		function() Noble.transition(ExampleScene, 1, Noble.TransitionType.CROSS_DISSOLVE) end,
-		function() Noble.transition(ExampleScene, 1, Noble.TransitionType.SLIDE_OFF_UP) end,
-		function() Noble.transition(ExampleScene, 1, Noble.TransitionType.SLIDE_OFF_DOWN) end,
-		function() Noble.transition(ExampleScene, 1, Noble.TransitionType.SLIDE_OFF_LEFT) end,
-		function() Noble.transition(ExampleScene, 1, Noble.TransitionType.SLIDE_OFF_RIGHT) end,
 		function()
 			local newValue = math.random(100,99999)
 			Noble.GameData.set("Score", newValue)
 			menuItemKeys[10] = "Change Score: " .. newValue
 		end
-	}
-
-	menu = UI.gridview.new(200, 10)
-	menu:setNumberOfColumns(1)
-	menu:setNumberOfRows(10)
-	menu:setCellPadding(0, 0, 0, 0)
-	menu.changeRowOnColumnWrap = false
-	menu:setSelectedRow(0)
-
-	function menu:drawCell(section, row, column, selected, x, y, width, height)
-		Noble.Text.setFont(Noble.Text.small)
-		if selected then
-			Graphics.setPattern({0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55})
-			Graphics.setLineWidth(5)
-			Graphics.drawLine(x, y+6, x+45, y+6)
-			Graphics.setLineWidth(1)
-			Graphics.setColor(Graphics.kColorBlack)
-			Graphics.drawLine(x, y+6, x+45, y+6)
-		end
-		Graphics.setImageDrawMode(Graphics.kDrawModeFillBlack)
-		Noble.Text.draw(menuItemKeys[row], x+50, y+2)
-	end
+	)
 
 	local crankTick = 0
 
 	ExampleScene2.inputHandler = {
 		upButtonDown = function()
-			menu:selectPreviousRow(true, false, false)
+			menu:selectPrevious()
 		end,
 		downButtonDown = function()
-			menu:selectNextRow(true, false, false)
+			menu:selectNext()
 		end,
 		cranked = function(change, acceleratedChange)
 			crankTick = crankTick + change
 			if (crankTick > 30) then
 				crankTick = 0
-				menu:selectNextRow(true, false, false)
+				menu:selectNext()
 			elseif (crankTick < -30) then
 				crankTick = 0
-				menu:selectPreviousRow(true, false, false)
+				menu:selectPrevious()
 			end
 		end,
 		AButtonDown = function()
-			menuHandlers[menu.selectedRow]()
+			menu:click()
 		end
 	}
 
@@ -111,19 +81,20 @@ function ExampleScene2:drawBackground()
 	ExampleScene2.super.drawBackground(self)
 
 	background:draw(0, 0)
-	Graphics.setColor(Graphics.kColorWhite)
-	Graphics.fillRoundRect(15, (sequence:get()*0.75)+15, 185, 125, 15)
-
-	Graphics.setColor(Graphics.kColorBlack)
-	Graphics.fillRoundRect(260, -20, 130, 65, 15)
-	logo:draw(275, 8)
-
 end
 
 function ExampleScene2:update()
 	ExampleScene2.super.update(self)
 
-	menu:drawInRect(15, sequence:get() or 100, 200, 110)
+	Graphics.setColor(Graphics.kColorWhite)
+	Graphics.setDitherPattern(0.2, Graphics.image.kDitherTypeScreen)
+	Graphics.fillRoundRect(15, (sequence:get()*0.75)+3, 185, 145, 15)
+	menu:draw(30, sequence:get()-15 or 100-15)
+
+	Graphics.setColor(Graphics.kColorBlack)
+	Graphics.fillRoundRect(260, -20, 130, 65, 15)
+	logo:setInverted(false)
+	logo:draw(275, 8)
 
 end
 
